da82afa6b0513f2b7fedf436a1b600d3e056359c
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2021-02-18 16:42:30 -0600

    Add missing move enum

---
+++ include/mylibrary/movement.h
@@ -0,0 +1,14 @@
+//
+// Created by Emily on 4/18/2020.
+//
+
+#ifndef FINALPROJECT_MOVEMENT_H
+#define FINALPROJECT_MOVEMENT_H
+
+namespace myapp {
+
+enum class Movement { kJump, kLeft, kRight, kIdle};
+
+}
+
+#endif  // FINALPROJECT_MOVEMENT_H
