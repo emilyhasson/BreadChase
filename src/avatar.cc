c73c90dccc310fcf5b7ef737379d31de980d971c
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2021-02-18 16:38:48 -0600

    Create avatar.cc

---
+++ src/avatar.cc
@@ -0,0 +1,21 @@
+//
+// Created by Emily Hasson on 4/18/2020.
+//
+
+#include "mylibrary/avatar.h"
+
+namespace myapp {
+
+float Avatar::XCoord() {
+  return location_x;
+}
+
+float Avatar::YCoord() {
+  return location_y;
+}
+
+float Avatar::Size() {
+  return size;
+}
+
+}
