30b63de0f90a7eaed09b1771f4e797afb7110b06
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2021-02-18 16:43:50 -0600

    Update map

--- include/mylibrary/map.h
+++ include/mylibrary/map.h
@@ -6,29 +6,40 @@
 #define FINALPROJECT_MAP_H
 
 #include "cinder/gl/Texture.h"
+const int kTileSize = 32;
+static const int kNumRows = 23;
+static const int kNumColumns = 50;
+const int kGround = 0;
+const int kPlatform = 1;
+const int kSky = 2;
+const int kBread = 3;
+const int kEnemy = 4;
+const int kCloud = 5;
+const int kPoison = 6;
+const int kMagic = 7;
+
+namespace myapp {
 
 class Map {
 
-//  const int kTileSize = 32;
-
  public:
-
   Map();
-  ~Map();
-  void LoadMap(int map_array[20][25]);
+  Map(std::vector<std::string> map_file);
+  void LoadMap(int map_array[kNumRows][kNumColumns]);
   int GetMapArrayValue(int row, int column);
   ci::gl::Texture2dRef GetPlatformTexture();
   ci::gl::Texture2dRef GetGroundTexture();
   ci::gl::Texture2dRef GetSkyTexture();
+  ci::gl::Texture2dRef GetCloudTexture();
+
 
  private:
-
   ci::Rectf placement;
   ci::gl::Texture2dRef ground;
   ci::gl::Texture2dRef platform;
   ci::gl::Texture2dRef sky;
-  int map[20][25];
-
+  ci::gl::Texture2dRef cloud;
+  int map[kNumRows][kNumColumns];
 };
-
+}
 #endif  // FINALPROJECT_MAP_H
