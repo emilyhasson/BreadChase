9e6ca3f7acb7c39fb91c70a0a43ab1866d24b2db
Emily Hasson <55060753+emilyhasson@users.noreply.github.com>
2021-02-18 16:37:52 -0600

    Update bread.cc

--- src/bread.cc
+++ src/bread.cc
@@ -1,5 +1,25 @@
 //
-// Created by Emily on 4/18/2020.
+// Created by Emily Hasson on 4/18/2020.
 //
 
-#include <mylibrary/bread.h>
+#include "mylibrary/bread.h"
+
+#include <cinder/app/AppBase.h>
+#include <cinder/gl/Texture.h>
+
+namespace myapp {
+
+Bread::Bread() {}
+
+ci::gl::Texture2dRef Bread::GetBreadTexture() {
+  auto image = cinder::loadImage( cinder::app::loadAsset( bread_image_path ) );
+  ci::gl::Texture2dRef bread_texture = ci::gl::Texture2d::create(image);
+  return bread_texture;
+}
+
+void Bread::IsEaten() {
+  is_eaten = true;
+  bread_image_path = "../assets/sky2.png";
+}
+
+}
